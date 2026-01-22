b0cd9ba3760cf06799b49cbe57fb676770b8f11c
SusiBaril <rosaleskeegan@gmail.com>
2026-01-22 14:34:08 +0800

    first commit

---
+++ client/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+    cors: {
+      origin: 'http://localhost:3000',
+      credentials: true
+    }
+  },
+  proxy: {
+    '/api': 'http://localhost:8000',
+  },
+})
